120d13d446dbfad6babd33e8965c38540755e4cb
cricket94 <krystian.mazela@onet.pl>
2016-10-05 16:52:33 +0200

    template node crypto connection

--- i_crypto.hpp
+++ i_crypto.hpp
@@ -8,13 +8,20 @@ class i_crypto
 public:
     virtual std::vector <uint8_t> calculate_hash(const std::array<uint8_t, 16> & sender_address,
                                          const std::array<uint8_t, 16> & recivier_address,
-                                         size_t diff) = 0;
+                                         size_t diff) const = 0;
     virtual bool calculate_diff(const std::array<uint8_t, 16> & sender_address,
                                          const std::array<uint8_t, 16> & recivier_address,
-                                         size_t padding, size_t diff) = 0;
+                                         size_t padding, size_t diff) const = 0;
+    virtual std::array <uint8_t, 16> generate_address () const = 0;
+
 
     virtual ~i_crypto() = default;
 
 };
 
+
+class crypto_sha512: public i_crypto
+{
+    crypto_sha512();
+};
 #endif // I_CRYPTO_HPP
--- node.hpp
+++ node.hpp
@@ -3,13 +3,22 @@
 #include <vector>
 #include <memory>
 #include "connection.hpp"
+#include "i_crypto.hpp"
 
-class node
+
+class node final
 {   
 public:
     node();
+    void add_connection(const std::shared_ptr< node > & peer );
+    void del_connection(const std::shared_ptr< node > & peer );
+    std::array <uint8_t, 16> get_address() const;
+
 private:
     std::vector< std::weak_ptr <connection> > m_connections;
+    std::unique_ptr <i_crypto> m_crypto;
+    std::array <uint8_t, 16> m_address;
+    size_t diff;
 
 };
 
